f65c9758543797197027569e2863d221443156fe
philipmo <philipmo59@gmail.com>
2023-02-22 16:08:49 -0500

    added math

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 9
+let num4 = 31
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1
+const mod = num4 % num3
+const max = Math.max(5,4,20)
